6e1bc80afb6dba7d1c323c0a241920aa5719a407
sach1ng1 <saching1@umd.edu>
2026-03-15 22:57:05 -0400

    Created visualizations from model results and main file

--- vis/visualizations.py
+++ vis/visualizations.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 os.makedirs("data/visualizations",exist_ok=True)
 def read_model_outputs():
     crash_model_results=pd.read_csv("data/model_outputs/crash_model_prediction_results.csv")
@@ -11,11 +12,13 @@ def read_model_outputs():
 def static_viz(crash_model_results, crash_predictor_results):
     plt.figure(figsize=(10,8))
     sns.pairplot(data=crash_model_results[["actual_crash_data","predicted_crash_data"]])
+    plt.savefig("data/visualizations/model_pairplot.png")
     plt.show()
     
     
     plt.figure(figsize=(10,8))
     sns.barplot(data=crash_predictor_results, x="predictor", y="coefficient_impact")
+    plt.savefig("data/visualizations/model_barplot.png")
     plt.show()
     
     print("Summary of model results:")
@@ -24,8 +27,8 @@ def static_viz(crash_model_results, crash_predictor_results):
 
     
 
-#crash_model_results, crash_predictor_results=read_model_outputs()
-#static_viz(crash_model_results, crash_predictor_results)
+crash_model_results, crash_predictor_results=read_model_outputs()
+static_viz(crash_model_results, crash_predictor_results)
 
 
 # In the future I would like to create some other static visuals, will need to work on other options after
