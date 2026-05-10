a13e0702738a782aa977a75db34b853e5185ae63
sach1ng1 <saching1@umd.edu>
2026-05-09 20:55:36 -0400

    Futher accepted commits from test

--- vis/visualizations.py
+++ vis/visualizations.py
@@ -14,12 +14,6 @@ def read_model_outputs():
                 dataframe for the logistic regression model results
             crash_predictor_results:
                 dataframe for the logistic regression predictor results       
-<<<<<<< HEAD
-    """   
-    crash_model_results=pd.read_csv("data/model_outputs/crash_model_prediction_results.csv")
-    crash_predictor_results=pd.read_csv("data/model_outputs/predictor_impact_results.csv")
-    return crash_model_results, crash_predictor_results
-=======
     """
     try:   
         crash_model_results=pd.read_csv("data/model_outputs/crash_model_prediction_results.csv")
@@ -27,7 +21,6 @@ def read_model_outputs():
         return crash_model_results, crash_predictor_results
     except:
         print("An exception has occured")
->>>>>>> test
 
 def static_viz_LR(crash_model_results, crash_predictor_results):
     """ 
@@ -39,24 +32,6 @@ def static_viz_LR(crash_model_results, crash_predictor_results):
             crash_predictor_results: dataframe
                 dataframe for the logistic regression predictor results       
     """ 
-<<<<<<< HEAD
-    
-    plt.figure(figsize=(10,8))
-    sns.pairplot(data=crash_model_results[["actual_crash_data","predicted_crash_data"]])
-    plt.savefig("data/visualizations/model_pairplot.png")
-    plt.show()
-    
-    
-    plt.figure(figsize=(10,8))
-    sns.barplot(data=crash_predictor_results, x="predictor", y="coefficient_impact")
-    plt.savefig("data/visualizations/model_barplot.png")
-    plt.show()
-    
-    print("Summary of model results:")
-    print("light seems to negatively impact crash realted injuries at a coefficient of about -0.25")
-    print("day of the week shows a low impact on crash related injury at a coefficeint of about 0.10")
-
-=======
     try:
         plt.figure(figsize=(10,8))
         sns.pairplot(data=crash_model_results[["actual_crash_data","predicted_crash_data"]])
@@ -74,7 +49,6 @@ def static_viz_LR(crash_model_results, crash_predictor_results):
         print("day of the week shows a low impact on crash related injury at a coefficeint of about 0.10")
     except:
         print("An exception has occured")
->>>>>>> test
 def static_viz_RF(rand_forest_model, feature_names):
     """ 
     Creates the tree plot for the random forest model 
@@ -84,15 +58,6 @@ def static_viz_RF(rand_forest_model, feature_names):
                 The crash data random forest model 
             feature_names: series
                 A series of feature names used in the random forest model
-<<<<<<< HEAD
-    """ 
-    for tree in range(3):
-        plt.figure(figsize=(15,20))
-        plot_tree(rand_forest_model.estimators_[tree], feature_names=feature_names, filled= True, max_depth=2)
-        plt.savefig(f"data/visualizations/random_forest_tree_{tree+1}.png")
-        plt.close()
-
-=======
     """
     try: 
         for tree in range(3):
@@ -103,4 +68,3 @@ def static_viz_RF(rand_forest_model, feature_names):
 
     except:
         print("An exception has occured")
->>>>>>> test
